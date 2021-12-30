07cbd0f0cb66b4d7e0113218bf659eea909bdb50
CHAOS2614 <37375653+CHAOS2614@users.noreply.github.com>
2021-12-30 09:56:57 +0800

    offer25 合并排序链表

---
+++ condition-offer/src/main/java/cn/edu/bjfu/algorithm/Offer25.java
@@ -0,0 +1,50 @@
+package cn.edu.bjfu.algorithm;
+
+/**
+ * @author chaos
+ * @date 2021-12-30 9:39
+ * <a href="https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/">合并两个排序的链表</a>
+ * <p>
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ * </p>
+ */
+public class Offer25 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                p.next = l2;
+                l2 = l2.next;
+            } else {
+                p.next = l1;
+                l1 = l1.next;
+            }
+            p = p.next;
+        }
+        if (l1 != null) {
+            p.next = l1;
+        }
+        if (l2 != null) {
+            p.next = l2;
+        }
+        return head.next;
+    }
+}
